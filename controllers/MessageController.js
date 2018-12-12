f600b1e5a52de770c24de060e4bd50aa142f4109
Mars Cheung <mars.kwong.cheung@gmail.com>
2018-12-12 14:26:48 -0800

    refactor creating new one on one channel messages to use axios async await

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -9,7 +9,7 @@ const jwt = require("jsonwebtoken");
 const url = require('url');
 var async = require('async');
 
-exports.createMessage = (req, res, next) => {
+exports.createMessage = async(req, res, next) => {
 
 	let userId = '';
 	jwt.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode){
@@ -122,105 +122,66 @@ exports.createMessage = (req, res, next) => {
 			return res.json(numAff);	
 		});
 	} else {
-		async.series([
-			function(callback){
-				Channel.findOne({
-					_id:req.body.channelId
-				}, function(err,channel){
-	                if (err){
-	                	return callback(err);
-	                } 
 
-	                //Check that a user was found
-	                if (!channel) {
-	                    return callback(new Error('No Channel found.'));
-	                }
-	                selectedChannel = channel;
-	                callback();
-	            });
-			},
-			function(callback){
-				User.findOne({
-					_id:userId
-				}, function(err,user){
-	                if (err) return callback(err);
-	                //Check that a user was found
-	                if (!user) {
-	                    return callback(new Error('No User found.'));
-	                }
-	                currentUser = user;
-	                callback();
-	            });
-			},
-			function(callback){
-				const newMessage = new Message({
-					message: req.body.message,
-					user: currentUser
-				});
+		try {
 
-				newMessage.save(function(err,message){
-					if (err) return callback(err);
-	                if (!message) {
-	                    return callback(new Error('Message save unsuccessful.'));
-	                }
-	                savedMessage = message;
-	                callback();
-	            });
-			},
-			function(callback){
-				let allChannelMessages = selectedChannel.messages;
-				allChannelMessages.push(savedMessage._id);
-				Channel.update({
-					_id: req.body.channelId
-				},{
-					messages:allChannelMessages
-				}, function(err, response){
-					if (err) return callback(err);
-		            if (!response) {
-		                return callback(new Error('Channel message update unsuccessful.'));
-		            }
-					callback();
-				});
-			},
-			function(callback){	
+			const selectedChannel = await Channel.findOne({ _id:req.body.channelId });
 
-				MsgCount.findOne({
-					channel:req.body.channelId,
-					sender:userId
-				}, function(err,oneOnOneMsgCount){
-	                if (err) return callback(err);
-	                //Check that a user was found
-	                if (!oneOnOneMsgCount) {
-	                    return callback(new Error('No oneOnOne MsgCount found.'));
-	                }
-	                msgCount_1on1 = oneOnOneMsgCount;
-	                callback();
-				})
-			},
-			function(callback){	
+            if (!selectedChannel) {
+                throw new Error('No Channel found.');
+            }
 
-				let numMessagesToUser = msgCount_1on1.messageCount;
-				numMessagesToUser++;
-				MsgCount.update({
-					_id: msgCount_1on1._id
-				},{
-					messageCount:numMessagesToUser
-				}, function(err, response){
-					if (err) return callback(err);
-	                if (!response) {
-	                    return callback(new Error('Channel message count update unsuccessful.'));
-	                }
-					callback();
-				});
-			},
+			const currentUser = await User.findOne({ _id:userId });
 
-		], function(err, numAff, response) {
-			if(err){
-				// res.status(401).json({ message: 'Error with channel message input.' });
-				return next(err);
-			}							
-			return res.json(numAff);	
-		});
+            if (!currentUser) {
+                throw new Error('No User found.');
+            }
+
+			const newMessage = new Message({
+				message: req.body.message,
+				user: currentUser
+			});
+
+			const savedMessage = await newMessage.save();
+
+			let allChannelMessages = selectedChannel.messages;
+			allChannelMessages.push(savedMessage._id);
+
+			const channelUpdateResp = await Channel.update({
+				_id: req.body.channelId
+			},{
+				messages:allChannelMessages
+			});
+
+            if (!channelUpdateResp) {
+                throw new Error('Channel message update unsuccessful.');
+            }
+
+			const msgCount_1on1 = await MsgCount.findOne({
+				channel:req.body.channelId,
+				sender:userId
+			});
+            if (!msgCount_1on1) {
+                throw new Error('No oneOnOne MsgCount found.');
+            }
+
+			let numMessagesToUser = msgCount_1on1.messageCount;
+			numMessagesToUser++;
+
+			const msgCountUpdateResp = await MsgCount.update({
+				_id: msgCount_1on1._id
+			},{
+				messageCount:numMessagesToUser
+			});
+
+            if (!msgCountUpdateResp) {
+                throw new Error('Channel message count update unsuccessful.');
+            }
+			return res.json();
+
+		} catch(error) {
+            console.log(error);
+		}
 	}
 } 
 
