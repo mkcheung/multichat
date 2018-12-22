c5e56647671b5a27f6ef3cc7dcfdac6fba9bdf3c
Mars Cheung <mars.kwong.cheung@gmail.com>
2018-12-21 23:53:53 -0800

    add authentication test for usercontroller login

--- tests/user.controller.test.js
+++ tests/user.controller.test.js
@@ -7,23 +7,24 @@ const chaiAsPromised = require('chai-as-promised');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const rewire = require('rewire');
+const jwt = require("jsonwebtoken");
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 
 var mongoose = require('mongoose');
 var User = rewire('../models/User');
 
-var UserController = require('../controllers/UserController');
+var UserController = rewire('../controllers/UserController');
 var sandbox = sinon.sandbox.create();
 
 describe('User Controller', ()=> {
 
 	let req;
-	let res;
-	let userFindResult;
+	let userToLogin;
+	let jwt;
 
     beforeEach(function() {
-		userFindResult = {
+		userToLogin = {
 			comparePassword: sandbox.stub().resolves(false)
 		};
 
@@ -43,6 +44,14 @@ describe('User Controller', ()=> {
 	        }
 	      }
 	    };
+
+	    let jwtMock = {
+	    	sign:function(){
+	    		return 'faeae';
+	    	}
+	    }
+
+UserController.__set__('jwt',jwtMock)
     });
 
     afterEach(() => {
@@ -60,11 +69,61 @@ describe('User Controller', ()=> {
 
 		it('should return bad credentials error after incorrect password', async () => {
 
-        	sandbox.stub(User, 'findOne').resolves(userFindResult);
+        	sandbox.stub(User, 'findOne').resolves(userToLogin);
 	        let result = await UserController.login(req, res);
 			expect(result.status).to.equal(401);
     		expect(result.response).to.have.property('message').to.equal('Bad credentials. User not found.');
 		});
+
+		it('should return user id and token on authentication', async () => {
+
+			userToLogin = {
+				_id:1,
+				email:'foo@bar.gmail.com',
+				firstName:'foo',
+				lastName:'bar',
+				token:'faeae',
+				comparePassword: sandbox.stub().resolves(true)
+			};
+
+	    	res = {
+		      json: (response) => {
+	            return { response: response };
+		      }
+		    };
+        	sandbox.stub(User, 'findOne').resolves(userToLogin);
+	        let result = await UserController.login(req, res);
+    		expect(result.response).to.have.property('userid').to.equal(userToLogin._id);
+    		expect(result.response).to.have.property('token').to.equal(userToLogin.token);
+		});
+
+		// it('should throw an exception if an issue occurs', async () => {
+
+		// 	let spy = sinon.spy();
+		// 	userToLogin = {
+		// 		_id:null,
+		// 		email:'foo@bar.gmail.com',
+		// 		firstName:'foo',
+		// 		lastName:'bar',
+		// 		token:'faeae',
+		// 		comparePassword: sandbox.stub().throws('randomError')
+		// 	};
+
+		// 	res = {
+		// 		status: (code) => {
+		// 			return {
+		// 				send: (error) => {
+		// 					return { status: code, response: response };
+		// 				}
+		// 			}
+		// 		}
+		// 	};
+
+  //       	sandbox.stub(User, 'findOne').resolves(userToLogin);
+	 //        let result = await UserController.login(req, res);
+		// 	expect(result.status).to.equal(400);
+  //       	expect(spy.calledOnce).to.equal(true);
+		// });
 	});
 
 
